0b9653f587e3dba693d9a5439b88d6e07b7c7fda
notestyle <notestyle@yahoo.com>
2018-10-16 21:11:55 +0800

    create template but root doesn't work

---
+++ gr-system/src/main.js
@@ -0,0 +1,16 @@
+import Vue from "vue";
+import App from "./App";
+import router from "./router";
+
+Vue.config.productionTip = false;
+
+// new Vue({
+//   render: h => h(App)
+// }).$mount("#app");
+
+new Vue({
+  el: "#app",
+  router: router,
+  components: { App },
+  template: "<App/>"
+});
