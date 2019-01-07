3b0230447712196c4859e9ba7e58193a1b608ca5
notestyle <notestyle@yahoo.com>
2019-01-07 21:55:12 +0800

    save

--- b-reservation/src/components/Navbar.vue
+++ b-reservation/src/components/Navbar.vue
@@ -1,67 +1,89 @@
 <template>
-<!-- partial:partials/_navbar.html -->
-    <nav class="navbar default-layout col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
-      <div class="text-center navbar-brand-wrapper d-flex align-items-top justify-content-center">
-        <a class="navbar-brand brand-logo" href="javascript;:">
-          <img src="/static/images/logo.svg" alt="logo" />
-        </a>
-        <a class="navbar-brand brand-logo-mini" href="javascript;:">
-          <img src="/static/images/logo-mini.svg" alt="logo" />
-        </a>
-      </div>
-      <div class="navbar-menu-wrapper d-flex align-items-center">
-        <ul class="navbar-nav navbar-nav-right">
-          <li class="nav-item dropdown d-none d-xl-inline-block">
-            <a class="nav-link dropdown-toggle" id="UserDropdown" href="#" data-toggle="dropdown" aria-expanded="false">
-              <span class="profile-text">admin</span>
-              <img class="img-xs rounded-circle" src="/static/images/faces/face1.jpg" alt="Profile image">
-            </a>
-            <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="UserDropdown">
-              <a class="dropdown-item p-0">
-                <div class="d-flex border-bottom">
-                  <div class="py-3 px-4 d-flex align-items-center justify-content-center">
-                    <i class="mdi mdi-bookmark-plus-outline mr-0 text-gray"></i>
-                  </div>
-                  <div class="py-3 px-4 d-flex align-items-center justify-content-center border-left border-right">
-                    <i class="mdi mdi-account-outline mr-0 text-gray"></i>
-                  </div>
-                  <div class="py-3 px-4 d-flex align-items-center justify-content-center">
-                    <i class="mdi mdi-alarm-check mr-0 text-gray"></i>
-                  </div>
+  <!-- partial:partials/_navbar.html -->
+  <nav class="navbar default-layout col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
+    <div class="text-center navbar-brand-wrapper d-flex align-items-top justify-content-center">
+      <a class="navbar-brand brand-logo" href="javascript;:">
+        <img src="/static/images/logo.svg" alt="logo">
+      </a>
+      <a class="navbar-brand brand-logo-mini" href="javascript;:">
+        <img src="/static/images/logo-mini.svg" alt="logo">
+      </a>
+    </div>
+    <div class="navbar-menu-wrapper d-flex align-items-center">
+      <ul class="navbar-nav navbar-nav-right">
+        <li class="nav-item dropdown d-none d-xl-inline-block">
+          <a
+            class="nav-link dropdown-toggle"
+            id="UserDropdown"
+            href="#"
+            data-toggle="dropdown"
+            aria-expanded="false"
+          >
+            <span class="profile-text">{{get_username}}</span>
+            <img
+              class="img-xs rounded-circle"
+              src="/static/images/faces/face1.jpg"
+              alt="Profile image"
+            >
+          </a>
+          <div
+            class="dropdown-menu dropdown-menu-right navbar-dropdown"
+            aria-labelledby="UserDropdown"
+          >
+            <a class="dropdown-item p-0">
+              <div class="d-flex border-bottom">
+                <div class="py-3 px-4 d-flex align-items-center justify-content-center">
+                  <i class="mdi mdi-bookmark-plus-outline mr-0 text-gray"></i>
                 </div>
-              </a>
-              <a class="dropdown-item mt-2">
-                Миний мэдээлэл
-              </a>
-              <a class="dropdown-item">
-                Нууц үг солих
-              </a>
-              <a class="dropdown-item">
-                Тохиргоо
-              </a>
-              <a v-on:click='logOut' href="javascript:;" class="dropdown-item">
-                Гарах
-              </a>
-            </div>
-          </li>
-        </ul>
-        <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
-          <span class="mdi mdi-menu"></span>
-        </button>
-      </div>
-    </nav>
-    <!-- partial -->
+                <div
+                  class="py-3 px-4 d-flex align-items-center justify-content-center border-left border-right"
+                >
+                  <i class="mdi mdi-account-outline mr-0 text-gray"></i>
+                </div>
+                <div class="py-3 px-4 d-flex align-items-center justify-content-center">
+                  <i class="mdi mdi-alarm-check mr-0 text-gray"></i>
+                </div>
+              </div>
+            </a>
+            <a class="dropdown-item mt-2">Миний мэдээлэл</a>
+            <a class="dropdown-item">Нууц үг солих</a>
+            <a class="dropdown-item">Тохиргоо</a>
+            <a v-on:click="logOut" href="javascript:;" class="dropdown-item">Гарах</a>
+          </div>
+        </li>
+      </ul>
+      <button
+        class="navbar-toggler navbar-toggler-right d-lg-none align-self-center"
+        type="button"
+        data-toggle="offcanvas"
+      >
+        <span class="mdi mdi-menu"></span>
+      </button>
+    </div>
+  </nav>
+  <!-- partial -->
 </template>
 
 <script>
 export default {
-  name: 'Navbar', 
-  methods:{
+  name: "Navbar",
+  data() {
+    return {
+      username: "",
+      role: ""
+    };
+  },
+  methods: {
     logOut() {
-      console.log('logout...')
-      this.$store.dispatch('set_login_status', 0);
-      this.$router.replace('/login')
+      console.log("logout...");
+      this.$store.dispatch("set_login_status", 0);
+      this.$router.replace("/login");
+    }
+  },
+  computed: {
+    get_username() {
+      return this.$store.getters.get_logged_user.username;
     }
   }
-}
+};
 </script>
--- b-reservation/src/components/SideBar.vue
+++ b-reservation/src/components/SideBar.vue
@@ -1,62 +1,73 @@
 <template>
   <!-- partial:partials/_sidebar.html -->
-      <nav class="sidebar sidebar-offcanvas" id="sidebar">
-        <ul class="nav">
-          <li class="nav-item">
-            <router-link class="nav-link" v-bind:to="{ name: 'dashboard'}">
-              <i class="menu-icon fa fa-tachometer"></i>
-              <span class="menu-title">Хяналт</span>
-            </router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" v-bind:to="{ name: 'orders'}">
-              <i class="menu-icon mdi mdi-table"></i>
-              <span class="menu-title">Захиалга</span>
-            </router-link>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
-              <i class="menu-icon mdi mdi-content-copy"></i>
-              <span class="menu-title">Бүртгэл</span>
-              <i class="menu-arrow"></i>
-            </a>
-            <div class="collapse" id="ui-basic">
-              <ul class="nav flex-column sub-menu">
-                <li class="nav-item">
-                  <router-link class="nav-link" v-bind:to="{ name: 'rooms'}">
-                    <i class="menu-icon fa fa-home"></i>
-                    <span class="menu-title">Гэр / байшин</span>
-                  </router-link>
-                </li>
-                <li class="nav-item">
-                  <router-link class="nav-link" v-bind:to="{ name: 'room_categories'}">
-                    <i class="menu-icon fa fa-home"></i>
-                    <span class="menu-title">Гэр бүлэг</span>
-                  </router-link>
-                </li>
-                <li class="nav-item">
-                  <router-link class="nav-link" v-bind:to="{ name: 'services'}">
-                    <i class="menu-icon fa fa-money"></i>
-                    <span class="menu-title">Үйлчилгээ</span>
-                  </router-link>
-                  <!-- <a class="nav-link" href="java">Үйлчилгээ</a> -->
-                </li>
-              </ul>
-            </div>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" v-bind:to="{ name: 'report'}">
-              <i class="menu-icon fa fa-bar-chart-o"></i>
-              <span class="menu-title">Тайлан</span>
-            </router-link>
-          </li>
-        </ul>
-      </nav>
-      <!-- partial -->
+  <nav class="sidebar sidebar-offcanvas" id="sidebar">
+    <ul class="nav">
+      <li class="nav-item">
+        <router-link class="nav-link" v-bind:to="{ name: 'dashboard'}">
+          <i class="menu-icon fa fa-tachometer"></i>
+          <span class="menu-title">Хяналт</span>
+        </router-link>
+      </li>
+      <li class="nav-item">
+        <router-link class="nav-link" v-bind:to="{ name: 'orders'}">
+          <i class="menu-icon mdi mdi-table"></i>
+          <span class="menu-title">Захиалга</span>
+        </router-link>
+      </li>
+      <li class="nav-item" v-if="get_role === 'admin'">
+        <a
+          class="nav-link"
+          data-toggle="collapse"
+          href="#ui-basic"
+          aria-expanded="false"
+          aria-controls="ui-basic"
+        >
+          <i class="menu-icon mdi mdi-content-copy"></i>
+          <span class="menu-title">Бүртгэл</span>
+          <i class="menu-arrow"></i>
+        </a>
+        <div class="collapse" id="ui-basic">
+          <ul class="nav flex-column sub-menu">
+            <li class="nav-item">
+              <router-link class="nav-link" v-bind:to="{ name: 'rooms'}">
+                <i class="menu-icon fa fa-home"></i>
+                <span class="menu-title">Гэр / байшин</span>
+              </router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" v-bind:to="{ name: 'room_categories'}">
+                <i class="menu-icon fa fa-home"></i>
+                <span class="menu-title">Гэр бүлэг</span>
+              </router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" v-bind:to="{ name: 'services'}">
+                <i class="menu-icon fa fa-money"></i>
+                <span class="menu-title">Үйлчилгээ</span>
+              </router-link>
+              <!-- <a class="nav-link" href="java">Үйлчилгээ</a> -->
+            </li>
+          </ul>
+        </div>
+      </li>
+      <li class="nav-item">
+        <router-link class="nav-link" v-bind:to="{ name: 'report'}">
+          <i class="menu-icon fa fa-bar-chart-o"></i>
+          <span class="menu-title">Тайлан</span>
+        </router-link>
+      </li>
+    </ul>
+  </nav>
+  <!-- partial -->
 </template>
 
 <script>
 export default {
-  name: 'SideBar', 
-}
+  name: "SideBar",
+  computed: {
+    get_role() {
+      return this.$store.getters.get_logged_user.role;
+    }
+  }
+};
 </script>
