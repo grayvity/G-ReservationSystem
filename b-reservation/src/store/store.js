b2f04a166018db8f0f00e7e2a88dde9f3781cd8c
notestyle <notestyle@yahoo.com>
2019-01-10 20:46:11 +0800

    fix login bug

--- b-reservation/src/store/store.js
+++ b-reservation/src/store/store.js
@@ -6,9 +6,10 @@ Vue.use(Vuex);
 export const store = new Vuex.Store({
   state: {
     is_logged: localStorage.getItem("token") || 0,
-    logged_user: localStorage.getItem("logged_user") || {},
+    logged_user: { username: localStorage.getItem("username") || '', role: localStorage.getItem("role") || '' },
     is_loading: false
   },
+
   getters: {
     get_is_logged: state => {
       return state.is_logged;
@@ -18,9 +19,11 @@ export const store = new Vuex.Store({
     },
     get_logged_user: state => {
       return state.logged_user;
-    }
+    },
+
   },
   mutations: {
+
     set_login_status: (state, is_logged) => {
       state.is_logged = is_logged;
       localStorage.setItem("token", is_logged);
@@ -29,7 +32,10 @@ export const store = new Vuex.Store({
       state.is_loading = is_loading;
     },
     set_logged_user: (state, logged_user) => {
-      state.logged_user = logged_user
+      state.logged_user = logged_user;
+
+      localStorage.setItem("username", logged_user.username);
+      localStorage.setItem("role", logged_user.role);
     }
   },
   actions: {
