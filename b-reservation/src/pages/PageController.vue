f57c043c825f2c32c748085dc94cb7ba5912d822
notestyle <notestyle@yahoo.com>
2019-01-15 14:49:26 +0800

    home page redirect to dashboard

--- b-reservation/src/pages/PageController.vue
+++ b-reservation/src/pages/PageController.vue
@@ -1,5 +1,5 @@
 <template>
-    <Container/>
+  <Container/>
 </template>
 
 <script>
@@ -7,18 +7,23 @@ import Container from "@/components/Container.vue";
 import Login from "@/pages/Login.vue";
 
 export default {
-  name: 'PageController',
-  components: {Container},
-  computed:{
-		get_is_logged(){
-			return this.$store.getters.get_is_logged;
-		}
-	},
-  mounted(){
-      
-    if(this.$store.getters.get_is_logged != 1){
-        this.$router.push('/login')
+  name: "PageController",
+  components: { Container },
+  computed: {
+    get_is_logged() {
+      return this.$store.getters.get_is_logged;
+    },
+    get_router: function() {
+      return this.$route;
+    }
+  },
+  mounted() {
+    if (this.$store.getters.get_is_logged != 1) {
+      this.$router.push("/login");
+    }
+    if (this.$route.path === "/") {
+      this.$router.push("/dashboard");
     }
   }
-}
+};
 </script>
